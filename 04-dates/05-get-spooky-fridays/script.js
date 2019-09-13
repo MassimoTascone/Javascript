73af667f02cb89c7e4e2513a99db388c00d7a9cb
Massimo Tascone <tascone.massimo@gmail.com>
2019-09-13 16:45:51 +0200

    5-2

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -14,25 +14,18 @@
     document.getElementById("run").addEventListener("click" ,() =>{
 
         let year = document.getElementById("year").value;
+        let arrayAnne = [];
         for (i=0; i<=11; i++){
             let annee = new Date(year, i, 13);
             // Grace a une loop j'ai recup tous les 13 du mois
             console.log(annee);
             let day = annee.getDay();
             if (day == 5) {
-                alert(annee);
+                arrayAnne.push(annee);
             }
+            
         }
-
-
-    
-
-
-
-
-
-
-
+        alert(arrayAnne);
 
     })
 
